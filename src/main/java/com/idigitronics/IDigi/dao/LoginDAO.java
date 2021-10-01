c61ff2a1131872e15145e1b662005614e15bb805
DELL <DELL@VmL-PC>
2021-10-01 16:55:04 +0530

    minor changes related to diplay view

--- src/main/java/com/idigitronics/IDigi/dao/LoginDAO.java
+++ src/main/java/com/idigitronics/IDigi/dao/LoginDAO.java
@@ -86,10 +86,25 @@ public class LoginDAO {
 							userDetails.setBlockName(resultSet.getString("BlockName"));
 							userDetails.setID(resultSet.getInt("ID"));
 							
-							PreparedStatement pstmt4 = con.prepareStatement("SELECT * FROM customermeterdetails WHERE <id> = ");
+							String viewQuery = "SELECT DISTINCT cmd.MeterType FROM customerdetails AS cd LEFT JOIN customermeterdetails AS cmd ON cd.CustomerID = cmd.CustomerID WHERE cmd.MeterType IN ('Gas', 'Water', 'Energy') <id>";
+							PreparedStatement pstmt4 = con.prepareStatement(viewQuery.replace("<id>", (userDetails.getRoleID() == 1 || userDetails.getRoleID() == 4) ? "" : (userDetails.getRoleID() == 2 || userDetails.getRoleID() == 5) ? "AND cd.BlockID = "+userDetails.getBlockID() : (userDetails.getRoleID() == 3) ? "AND cd.CustomerID = "+userDetails.getCustomerID() : ""));
 							ResultSet rs4 = pstmt4.executeQuery();
 							
 							if(rs4.next()) {
+								
+								if(rs4.getString("MeterType").equalsIgnoreCase("Gas") ) {
+									userDetails.setGas(true);
+									rs4.next();
+								}
+								
+								if (rs4.getString("MeterType").equalsIgnoreCase("Water")) {
+								userDetails.setWater(true);
+								rs4.next();
+								}
+								if(rs4.getString("MeterType").equalsIgnoreCase("Energy")) {
+									userDetails.setEnergy(true);
+									rs4.next();
+								}
 								
 							}
 
