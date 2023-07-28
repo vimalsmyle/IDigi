5bfea5d70fc1cb6eaa99f5cd47384898b072df58
DELL <DELL@192.168.1.6>
2023-07-28 15:22:25 +0530

    changes related to email

--- src/main/java/com/idigitronics/IDigi/dao/ExtraMethodsDAO.java
+++ src/main/java/com/idigitronics/IDigi/dao/ExtraMethodsDAO.java
@@ -95,15 +95,20 @@ public class ExtraMethodsDAO {
 //		props.put("mail.smtp.port", "587");
 		
 		// for gmail 
-		props.put("mail.smtp.host", "smtp.gmail.com");
+		/*props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "465");
 		props.put("mail.smtp.auth", "true");
-//		props.put("mail.smtp.starttls.enable", true);
+		props.put("mail.smtp.starttls.enable", true);
 		props.put("mail.smtp.socketFactory.port", "465");
 		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.socketFactory.fallback", "false");
-		props.put("mail.debug", "true");
+		props.put("mail.debug", "true");*/
 		
+		props.put("mail.smtp.host", "smtp.gmail.com");
+	    props.put("mail.smtp.port", "587");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+	    props.put("mail.smtp.starttls.enable", "true");
 		
 
 		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
