2f5cb302bc486d0b00c800c7dfd7a24bd599f8e1
vml <vml@localhost>
2026-06-10 22:56:00 +0530

    added annotations in login only

--- src/main/java/com/idigitronics/IDigi/controller/LoginController.java
+++ src/main/java/com/idigitronics/IDigi/controller/LoginController.java
@@ -22,13 +22,22 @@ import com.idigitronics.IDigi.request.vo.UserManagementRequestVO;
 import com.idigitronics.IDigi.response.vo.ResponseVO;
 import com.idigitronics.IDigi.utils.Encryptor;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 /**
  * @author K VimaL Kumar
  * 
  */
 @Controller
+@Api(tags = "Login Related APIs")
 public class LoginController {
 
+	@ApiOperation(value = "validates user for login")
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully Logged In")})
 	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	public @ResponseBody
 	ResponseVO validateUser(@RequestBody LoginVO loginvo)
@@ -49,10 +58,12 @@ public class LoginController {
 		
 		return responsevo;
 	}
-
+	
+	@ApiOperation(value = "Forgot Password")
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "Password has been sent to your registered email id")})
 	@RequestMapping(value = "/forgotpassword/{userid}", method = RequestMethod.POST, produces = "application/json")
 	public @ResponseBody
-	ResponseVO forgotpassword(@PathVariable("userid") String userid)
+	ResponseVO forgotpassword(@ApiParam(value = "user id") @PathVariable("userid") String userid)
 			throws ClassNotFoundException, BusinessException, SQLException {
 
 		LoginBO loginbo = new LoginBO();
@@ -73,6 +84,8 @@ public class LoginController {
 		return responsevo;
 	}
 	
+	@ApiOperation(value = "Change Password")
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "Password Updated Successfully")})
 	@RequestMapping(value = "/changepassword/{userID}", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	public @ResponseBody
 	ResponseVO changepassword(@PathVariable("userID") String userid,
